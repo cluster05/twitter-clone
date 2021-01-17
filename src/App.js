da16bbafd3d0075e45e24fc738655dd22eab0f2b
cluster05 <ajayakumbhar1234@gmail.com>
2021-01-17 21:11:48 +0530

    added font and manage google register screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
+import Register from "./components/Register/Register";
 import Layout from "./containers/Layout/Layout";
 import Sidenav from "./containers/Sidenav/Sidenav";
 
 const App = () => {
+
+    const [authState, setAuthState] = useState(false);
+
     return (
         <Router>
+            { !authState ?
+                <Register goWithGoogle={() => setAuthState(true)} /> :
+                null
+            }
             <div className="bg-gray-900 flex w-screen h-screen">
                 <Sidenav />
                 <Layout />
             </div>
-
         </Router>
     )
 }
